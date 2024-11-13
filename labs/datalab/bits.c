39f9e8b409f199b7b80dc122538fe8d4a1aecdcc
Frank Tian <tianyawen201209@hotmail.com>
2024-11-12 19:38:20 -0500

    Conditional

--- labs/datalab/bits.c
+++ labs/datalab/bits.c
@@ -217,7 +217,9 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+  int mask = !!x;
+  mask = ~mask + 1;
+  return (y & mask) | (z & ~mask);
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
